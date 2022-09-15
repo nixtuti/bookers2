e967883e1b9b6ca802bff571009314948afef11f
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-09-15 12:43:11 +0000

    [Add]bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root :to => 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+  devise_for :users
+  
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :users, only: [:index, :show, :edit]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
