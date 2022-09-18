02bfcfef23d2d71d29a7a78e384e37edc0e4e458
EC2 Default User <111040404+nixtuti@users.noreply.github.com>
2022-09-18 15:02:03 +0000

    [Update]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,15 @@ class UsersController < ApplicationController
   end
   
   def update
-    
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
 end
